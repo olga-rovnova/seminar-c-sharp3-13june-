ec1d07a029f3a7690ee9f55fdb86f0e9099679c3
Olga <olga-rovnova@yandex.ru>
2022-06-16 22:33:12 +0300

    homework task23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count<n)
+{
+    int result = Cube(count);
+    count++;
+    Console.Write($"{result}, ");
+}
+Console.WriteLine(Cube(n)+".");
+
+int Cube(int a)
+{
+    return a*a*a;
+}
